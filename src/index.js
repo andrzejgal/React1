abe2175b1936e5d95915f79ac0ac1785bbeca486
andrzejgal <andrzejmg@op.pl>
2021-03-26 12:28:15 +0100

    poprawka index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Test from './components/test-component'
+import Test from './components/test-component/test.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,3 +16,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
